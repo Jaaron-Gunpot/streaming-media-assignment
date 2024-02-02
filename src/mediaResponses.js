506ab4650d90075804d7e3b7cf5c548b3d3058a0
Jaaron <jsw6194@rit.edu>
2024-02-02 18:24:27 -0500

    passed the tests

--- src/mediaResponses.js
+++ src/mediaResponses.js
@@ -1,69 +1,68 @@
 const fs = require('fs');
 const path = require('path');
 
-//the requested streamed file must be in the client folder
-const streamMedia = (nameOfFile,type,request,response) =>{
-    const file = path.resolve(__dirname, `../client/${nameOfFile}`);
+// the requested streamed file must be in the client folder
+const streamMedia = (nameOfFile, type, request, response) => {
+  const file = path.resolve(__dirname, `../client/${nameOfFile}`);
 
-    fs.stat(file, (err, stats) => {
-        if (err) {
-            if (err.code === 'ENOENT') {
-                response.writeHead(404);
-            }
-            return response.end(err);
-        }
+  fs.stat(file, (err, stats) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        response.writeHead(404);
+      }
+      return response.end(err);
+    }
 
-        let { range } = request.headers;
-        if (!range) {
-            range = 'bytes=0-';
-        }
+    let { range } = request.headers;
+    if (!range) {
+      range = 'bytes=0-';
+    }
 
-        const positions = range.replace(/bytes=/, '').split('-');
+    const positions = range.replace(/bytes=/, '').split('-');
 
-        let start = parseInt(positions[0], 10);
+    let start = parseInt(positions[0], 10);
 
-        const total = stats.size;
+    const total = stats.size;
 
-        const end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+    const end = positions[1] ? parseInt(positions[1], 10) : total - 1;
 
-        if (start > end) {
-            start = end - 1;
-        }
+    if (start > end) {
+      start = end - 1;
+    }
 
-        const chunksize = (end - start) + 1;
-        response.writeHead(206, {
-            'Content-Range': `bytes ${start}-${end}/${total}`,
-            'Accept-Ranges': 'bytes',
-            'Content-Length': chunksize,
-            'Content-Type': `${type}`,
-        });
-
-        const stream = fs.createReadStream(file, { start, end });
-
-        stream.on('open',()=>{
-            stream.pipe(response);
-        });
-
-        stream.on('error', (streamErr) => {
-            response.end(streamErr);
-        });
-
-        return stream;
+    const chunksize = (end - start) + 1;
+    response.writeHead(206, {
+      'Content-Range': `bytes ${start}-${end}/${total}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': `${type}`,
     });
+
+    const stream = fs.createReadStream(file, { start, end });
+
+    stream.on('open', () => {
+      stream.pipe(response);
+    });
+
+    stream.on('error', (streamErr) => {
+      response.end(streamErr);
+    });
+
+    return stream;
+  });
 };
 
 const getParty = (request, response) => {
-    streamMedia('party.mp4','video/mp4',request,response);
+  streamMedia('party.mp4', 'video/mp4', request, response);
 };
 
 const getBling = (request, response) => {
-    streamMedia('bling.mp3','audio/mpeg',request,response);
+  streamMedia('bling.mp3', 'audio/mpeg', request, response);
 };
 
 const getBird = (request, response) => {
-    streamMedia('bird.mp4','video/mp4',request,response);
+  streamMedia('bird.mp4', 'video/mp4', request, response);
 };
-
 
 module.exports.getParty = getParty;
 module.exports.getBling = getBling;
--- src/server.js
+++ src/server.js
@@ -1,27 +1,26 @@
 const http = require('http');
 const htmlHandler = require('./htmlResponses.js');
 const mediaHandler = require('./mediaResponses.js');
-const { url } = require('inspector');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const urlStruct = {
-    '/': htmlHandler.getIndex,
-    '/party.mp4': mediaHandler.getParty,
-    '/bling.mp3': mediaHandler.getBling,
-    '/bird.mp4': mediaHandler.getBird,
-    '/page2': htmlHandler.getPage2,
-    '/page3': htmlHandler.getPage3,
+  '/': htmlHandler.getIndex,
+  '/party.mp4': mediaHandler.getParty,
+  '/bling.mp3': mediaHandler.getBling,
+  '/bird.mp4': mediaHandler.getBird,
+  '/page2': htmlHandler.getPage2,
+  '/page3': htmlHandler.getPage3,
 };
 
 const onRequest = (request, response) => {
   console.log(request.url);
-  //if the url is defined in the urlStruct object,it goes there. If not, it goes to the index page
-    if (urlStruct[request.url]) {
-        urlStruct[request.url](request, response);
-    } else {
-        urlStruct['/'](request, response);
-    }
+  // if the url is defined in the urlStruct object,it goes there. If not, it goes to the index page
+  if (urlStruct[request.url]) {
+    urlStruct[request.url](request, response);
+  } else {
+    urlStruct['/'](request, response);
+  }
 };
 
 http.createServer(onRequest).listen(port, () => {
